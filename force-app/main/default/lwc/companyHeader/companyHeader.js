4eb0038ecea78cbc56ead4f81cffd82eb9eecb72
Alexander D Steele <Alexander.Steele.2014@outlook.com>
2024-06-11 17:49:15 -0500

    Add CompanyHeader component for displaying company information in the header

---
+++ force-app/main/default/lwc/companyHeader/companyHeader.js
@@ -0,0 +1,33 @@
+import { LightningElement, wire } from 'lwc';
+import Id from '@salesforce/user/Id';
+import getCompany from '@salesforce/apex/CompanyPortalController.getCompany';
+import getUser from '@salesforce/apex/CompanyPortalController.getUser';
+
+export default class CompanyHeader extends LightningElement {
+
+    userId = Id;
+    user;
+    userName;
+    company;
+    companyName;
+
+    @wire(getUser, { userId: '$userId' })
+    wiredUser({ error, data }) {
+        if (data) {
+            this.user = data;
+            this.userName = this.user.Name;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+    @wire(getCompany, { userId: '$userId' })
+    wiredCompany({ error, data }) {
+        if (data) {
+            this.company = data;
+            this.companyName = this.company.Name;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+}
