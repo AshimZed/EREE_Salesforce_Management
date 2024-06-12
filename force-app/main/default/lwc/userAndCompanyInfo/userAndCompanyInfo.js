035ea676ee62875a13221b16ea2750a31a4e95ae
Alexander D Steele <Alexander.Steele.2014@outlook.com>
2024-06-11 20:07:47 -0500

    Add UserAndCompanyInfo component for displaying user and company information

---
+++ force-app/main/default/lwc/userAndCompanyInfo/userAndCompanyInfo.js
@@ -0,0 +1,43 @@
+import { LightningElement, wire } from 'lwc';
+import Id from '@salesforce/user/Id';
+import getUser from '@salesforce/apex/CompanyPortalController.getUser';
+import getCompany from '@salesforce/apex/CompanyPortalController.getCompany';
+
+export default class UserAndCompanyInfo extends LightningElement {
+
+    userId = Id;
+    name;
+    userName;
+    company;
+    companyName;
+    annualRevenue;
+    numberOfEmployees;
+    industry;
+    region;
+
+    @wire(getUser , {userId: '$userId'})
+    wiredUser({error, data}) {
+        if(data) {
+            this.name = data.Name;
+            this.userName = data.Username;
+        } else if(error) {
+            console.error(error);
+        }
+    }
+
+    @wire(getCompany , {userId: '$userId'})
+    wiredCompany({error, data}) {
+        if(data) {
+            this.company = data;
+            this.companyName = this.company.Name;
+            this.annualRevenue = this.company.AnnualRevenue;
+            this.numberOfEmployees = this.company.NumberOfEmployees;
+            this.industry = this.company.NAICS_Code__c;
+            this.region = this.company.Region__c;
+        } else if(error) {
+            console.error(error);
+        }
+    }
+            
+
+}
